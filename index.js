3772fbbb574f5f37af961eb4273457cef79cfe6f
Sudhak23 <sudhak805@gmail.com>
2024-02-10 22:21:03 +0530

    update button and delete functions added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var addbtn = document.getElementById("add-item");
 var todolist = document.getElementById("todo-list");
 var item1 = document.getElementById("item1");
 var inputvalue = document.getElementById("inputvalue")
+var btnUpdate=document.getElementById("update-item");
+var btnDelete = document.getElementById("remove-item");
 var currentinputvalue = '';
 
 inputvalue.addEventListener('input', function (e) {
@@ -13,21 +15,37 @@ inputvalue.addEventListener('keyup', function (e) {
     }
 
 });
+function createNewNode(){
+    var newlistelement = document.createElement('li');
+    var textnode=document.createTextNode(currentinputvalue);
+    newlistelement.appendChild(textnode);
+    newlistelement.id="item"+(todolist.childElementCount + 1);
+    return newlistelement
+    var newlistelement = document.createElement('li');
+        // var textnode = document.createTextNode(currentinputvalue)
+        // newlistelement.appendChild(textnode);
+        // newlistelement.id = "item" + (todolist.childElementCount + 1)
+
+
+}
 function addlistItem() {
     if (currentinputvalue !== '' && currentinputvalue !== undefined && currentinputvalue !== null) {
-        var newlistelement = document.createElement('li');
+        
         //  var textnode=document.createTextNode("list item "+((todolist.childElementCount)+1))
-        var textnode = document.createTextNode(currentinputvalue)
-        console.log(currentinputvalue);
-        newlistelement.appendChild(textnode);
-        newlistelement.id = "item" + (todolist.childElementCount + 1)
+        // var newlistelement = document.createElement('li');
+        // var textnode = document.createTextNode(currentinputvalue)
+        // newlistelement.appendChild(textnode);
+        // newlistelement.id = "item" + (todolist.childElementCount + 1)
 
 
         //var firstitem = todolist.firstElementChild
        // console.log(firstitem)
-        todolist.appendChild(newlistelement)
+      // todolist.appendChild(newlistelement)
         //todolist.insertBefore(newlistelement,item1)
         //todolist.insertBefore(newlistelement, firstitem)
+        var newlistelement = createNewNode();
+        todolist.appendChild(newlistelement)
+
         inputvalue.value=''
         currentinputvalue=''
       
@@ -40,4 +58,19 @@ console.log(currentinputvalue);
 
 
 addbtn.addEventListener('click', addlistItem);
+
+//Updating the list items
+
+
+
+btnUpdate.addEventListener('click',function(){
+    var firstElement=todolist.firstElementChild;
+    //creating new list element 
+    var newlistelement = createNewNode();
+    todolist.replaceChild(newlistelement,firstElement);
+});
+btnDelete.addEventListener('click',function(){
+    var firstElement= todolist.firstElementChild;
+    todolist.removeChild(firstElement);
+});
 
